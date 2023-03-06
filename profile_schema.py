8a84efbd56e00dfad57f4bea341245630f0e175f
adiletsaparbek <adilet.saparbek@data-ox.com>
2023-03-06 15:09:19 +0600

    update pydantic schema by summarize method

--- profile_schema.py
+++ profile_schema.py
@@ -26,3 +26,30 @@ class Profile(BaseModel):
     description: str
     location: Location
     experiences: List[Experience]
+
+    # method for summarizing total experience without overlapping
+    def summarize_experiences(self) -> list:
+        companies = []
+        current_company = self.experiences[0].company_name
+        current_start_date = self.experiences[0].starts_at
+        current_end_date = self.experiences[0].ends_at
+
+        for exp in self.experiences[:]:
+            if current_end_date is not None and current_end_date >= exp.starts_at:
+                current_end_date = max(current_end_date, exp.ends_at)
+            else:
+                companies.append({
+                    "company_name": current_company,
+                    "starts_at": current_start_date,
+                    "ends_at": current_end_date
+                })
+                current_company = exp.company_name
+                current_start_date = exp.starts_at
+                current_end_date = exp.ends_at
+
+        companies.append({
+            "company_name": current_company,
+            "starts_at": current_start_date,
+            "ends_at": current_end_date
+        })
+        return companies
